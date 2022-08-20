1c6d0575adebfb1e4852784e62eb3a66d1bf329d
pingerl <402434104@qq.com>
2022-08-20 20:09:30 +0800

    项目初始化+引入Router4

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { defineComponent, ref } from "vue";
+import { RouterView } from "vue-router";
+
+export const App = defineComponent({
+  setup() {
+    return () => (
+      <>
+        <header>
+          导航
+          <ul>
+            <li>
+              <router-link to="/">Foo</router-link>
+            </li>
+            <li>
+              <router-link to="/about">bar</router-link>
+            </li>
+          </ul>
+        </header>
+        <div>
+          <RouterView />
+        </div>
+      </>
+    );
+  },
+});
