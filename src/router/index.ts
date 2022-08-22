d5759830988ff64acb4a7933bb64e8f547ee4834
pingerl <402434104@qq.com>
2022-08-22 22:46:02 +0800

    feat：添加欢迎页面路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,22 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
+import { First } from "../views/welcome/components/First";
+import { Fourth } from "../views/welcome/components/Fourth";
+import { Second } from "../views/welcome/components/Second";
+import { Third } from "../views/welcome/components/Third";
+import { Welcome } from "../views/welcome/Welcome";
 const routes = [
-  { path: "/", component: Bar },
-  { path: "/about", component: Foo },
+  { path: "/", redirect: "/welcome" },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      { path: "", redirect: "/welcome/1" },
+      { path: "1", component: First },
+      { path: "2", component: Second },
+      { path: "3", component: Third },
+      { path: "4", component: Fourth },
+    ],
+  },
 ];
 
 const router = createRouter({
