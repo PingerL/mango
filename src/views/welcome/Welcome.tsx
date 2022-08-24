2839c22d40fb0f80ea33836a01921937b81dbf1e
pingerl <402434104@qq.com>
2022-08-24 10:21:45 +0800

    feat：添加路由动画

--- src/views/welcome/Welcome.tsx
+++ src/views/welcome/Welcome.tsx
@@ -1,7 +1,7 @@
-import { defineComponent } from "vue";
+import { defineComponent, Transition, VNode } from "vue";
 import s from "./Welcome.module.scss";
 import logo from "../../assets/logo.svg";
-import { RouterView } from "vue-router";
+import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 
 export const Welcome = defineComponent({
   setup() {
@@ -12,7 +12,24 @@ export const Welcome = defineComponent({
           <h1>芒果记账</h1>
         </header>
         <main class={s.main}>
-          <RouterView name="main" />
+          <RouterView name="main">
+            {({
+              Component: X,
+              route,
+            }: {
+              Component: VNode;
+              route: RouteLocationNormalizedLoaded;
+            }) => (
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveActiveClass={s.slide_fade_leave_active}
+              >
+                {X}
+              </Transition>
+            )}
+          </RouterView>
         </main>
         <footer>
           <RouterView name="footer" />
